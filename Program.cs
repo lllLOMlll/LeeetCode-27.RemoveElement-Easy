8919421190fce638d9249858fb59e4e156ded05b
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-22 20:26:44 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        s.RemoveElement([3,2,2,3], 3);
+
+        s.RemoveElement([0,1,2,2,3,0,4,2], 2);
+    }
+}
