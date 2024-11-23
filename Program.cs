57195841da76e3b758cc0437db340f02f7cb7274
Louis-Olivier Major <louis.olivier.major@gmail.com>
2024-11-23 11:16:57 -0500

    The variable return k is correct

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
 ﻿
-public class Solution {
-    public int RemoveElement(int[] nums, int val) {
-        return 0;
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums.Length == 0) return 0;
+
+        int indexToWrite = 0;
+        int elementsWihtouVal = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[indexToWrite] = nums[i];
+                elementsWihtouVal += 1;
+            }
+        }
+
+        return elementsWihtouVal;
     }
 
     static void Main(string[] args)
     {
         Solution s = new Solution();
-        s.RemoveElement([3,2,2,3], 3);
+        Console.WriteLine(s.RemoveElement([3, 2, 2, 3], 3));
 
-        s.RemoveElement([0,1,2,2,3,0,4,2], 2);
+        Console.WriteLine(s.RemoveElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
     }
 }
